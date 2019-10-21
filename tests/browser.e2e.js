15397f0e3b003c6977254db95cdbae662fa85c4f
shahin963 <samershahin963@gmail.com>
2019-10-21 12:57:12 +0300

    initail e2e tests

---
+++ tests/browser.e2e.js
@@ -0,0 +1,52 @@
+describe("e2e test", ()=> {
+
+	describe("Input validation", () => {
+
+		test('should not accept an empty Input', async () => {
+		  expect(true).toBeTruthy();
+		});
+
+		test('should not accept a nigative input', async ()=>{
+
+		});
+
+		test('should not accept a zero input', async () => {
+
+		});
+	});
+	
+	describe("Calculation validation", () => {
+
+		test('should calculate room area', async () => {
+
+		});
+
+		test('should calculate room area', async () => {
+
+		});
+
+		test('should calculate required number of tiles', () => {
+
+		});
+
+		test('should calculate actual are', () => {
+			
+		});
+	});
+
+	describe("Output validation", () => {
+
+		test('should display room Area', () => {
+
+		});
+
+		test('should display number of tiles needed', ()=> {
+
+		});
+
+		test('should display the actual tiles area', () => {
+
+		});
+	});
+
+});
